322db234c96231b0374e42e328b02369951c1e3f
Sylvester Akwaowo <ultrasoft101@gmail.com>
2021-08-18 22:41:28 +0000

    another push

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{ asset('styles.css') }}" rel="stylesheet">
+    <link href="/resources/css/app.css" rel="stylesheet">
     <!-- font awesome cdn link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
